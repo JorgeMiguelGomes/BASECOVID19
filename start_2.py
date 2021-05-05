2f317ace7b22ff702feb758e0afdf3ab6673d122
Luis Guimarais <luis.guimarais@volteuropa.org>
2021-05-06 00:09:33 +0100

    Cleared contracted and contracting dict. Some errors detected on origin file.

--- start_2.py
+++ start_2.py
@@ -1,5 +1,6 @@
 # Import relevant libraries 
 
+import numpy as np
 import pandas as pd # to deal with the dataset 
 import plotly.express as px #to plot with beauty 
 
@@ -28,4 +29,55 @@ for col_name in col_names_to_time:
 data["publicationDate"] = pd.to_datetime(data["publicationDate"])
 data["signingDate"] = pd.to_datetime(data["signingDate"])
 
-print(data[["signingDateParsed", "created", "updated"]].head())
+#print(data[["signingDateParsed", "created", "updated"]].head())
+
+## Fixing COntracting dictionary
+
+f = pd.DataFrame.from_dict(data['contracting'][0])
+dummy = f.copy(deep=True)
+dummy['nif'] = np.nan
+dummy['id'] = np.nan
+dummy['description'] = np.nan
+
+for i in range(1, len(data)):
+    try:
+        fprime = pd.DataFrame.from_dict(data['contracting'][i])
+    except:
+        fprime = dummy.copy(deep=True)
+        
+    f = pd.concat([f, fprime])
+    
+## Reset index for later merge
+f = f.reset_index(drop=True)
+
+## Rename columns to clarify names
+f = f.rename(columns={'nif':'contracting_nif', 'description':'contracting_description', 'id':'contracting_id'})
+
+## Fix contrated dictionary
+g = pd.DataFrame.from_dict(data['contracted'][0])
+
+for i in range(1, len(data)):
+    try:
+        ## Allocate only the first name where the contracted have issues
+        gprime = pd.DataFrame.from_dict(data['contracted'][i]).iloc[0, :].to_frame().T
+    except:
+        gprime = dummy.copy(deep=True)
+            
+    ## Add fixed row to contrated
+    g = pd.concat([g, gprime])
+    
+
+g = g.reset_index(drop=True)
+g = g.rename(columns={'nif':'contracted_nif', 'description':'contracted_description', 'id':'contracted_id'})
+
+## Merge Contractants and Contracted
+contracts = f.merge(g, left_index=True, right_index=True)
+
+## Add previously curated columns
+contracts['publicationDate'] = data['publicationDate']
+contracts['signingDateParsed'] = data['signingDateParsed']
+contracts['price'] = data['price']
+contracts['created'] = data['created']
+contracts['updated'] = data['updated']
+
+print(contracts.head())
